d1387b7076f260872b3cdfb4a19a027dc8080dab
Artur Sobczak <sobczak.artur88@gmail.com>
2023-01-16 21:04:59 +0100

    The program sort elements in array

--- src/com/sobczak/artur/Main.java
+++ src/com/sobczak/artur/Main.java
@@ -1,8 +1,27 @@
 package com.sobczak.artur;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("ffff");
+
+        int[] unsortedArray = {2, 10, 5, 3, 9, 7, 14, 12, 11};
+        System.out.println("Array before sort " + Arrays.toString(unsortedArray));
+        int miniElement = 0;
+        int tmpElement = 0;
+
+        for (int i = 0; i < unsortedArray.length; i++) {
+            miniElement = i;
+            for (int j = i; j < unsortedArray.length; j++){
+                if (unsortedArray[j] < unsortedArray[miniElement]){
+                    miniElement = j;
+                }
+            }
+            tmpElement = unsortedArray[i];
+            unsortedArray[i] = unsortedArray[miniElement];
+            unsortedArray[miniElement] = tmpElement;
+        }
+        System.out.println("Array after sort " + Arrays.toString(unsortedArray));
     }
 }
